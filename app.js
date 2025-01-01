5aa2e9aeffe54572e192aed98e800d891c9cc40c
SawZiDunn <szidunn199@gmail.com>
2025-01-01 13:26:46 +0700

    changes

--- app.js
+++ app.js
@@ -17,18 +17,17 @@ const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
 const authenticationMiddleware = require("./middleware/authentication");
 
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+    message: "Too many requests from this IP, please try again later.",
+});
+
 app.use(express.json());
 // extra packages
 app.use(helmet());
 app.use(cors());
-app.use(
-    rateLimit({
-        windowMs: 15 * 60 * 1000, // 15 minutes
-        limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
-        standardHeaders: "draft-8", // draft-6: `RateLimit-*` headers; draft-7 & draft-8: combined `RateLimit` header
-        legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
-    })
-);
+app.use(limiter);
 app.use(xss());
 // routes
 
